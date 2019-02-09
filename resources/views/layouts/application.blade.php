ed61035eaf9f3c99c42b1402df76480ba2484a7d
Odran HUSSON <ohusson55@gmail.com>
2019-02-09 11:57:38 +0100

    Materialize integration

---
+++ resources/views/layouts/application.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/materialize.js') }}" defer></script>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ asset('css/materialize.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+    <!-- header and menu -->
+    <nav class="light-blue lighten-1" role="navigation">
+        <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="#">Navbar Link</a></li>
+            </ul>
+
+            <ul id="nav-mobile" class="sidenav">
+                <li><a href="#">Navbar Link</a></li>
+            </ul>
+            <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        </div>
+    </nav>
+
+    @yield('content')
+
+    @yield('bottom-scripts')
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="{{ asset('js/materialize.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/init.js') }}"></script>
+</body>
+</html>
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,47 +1,71 @@
-@extends('layouts.app')
+@extends('layouts.application')
 
 @section('content')
+    <!-- Banner -->
+    <div class="section no-pad-bot" id="index-banner">
+        <div class="container">
+            <br><br>
+            <h1 class="header center orange-text">Tasks</h1>
+            <div class="row center">
+                <h5 class="header col s12 light">Create and automate bash tasks</h5>
+            </div>
+            <br><br>
+
+        </div>
+    </div>
+
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Tasks</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        <h1>Tasks</h1>
-                        <ul>
-                        @foreach($tasks as $task)
-                            <li>{{ $task->name }}</li>
-                            <li>Variables</li>
-                            <li><ul>
-                            @foreach($task->variables as $variable)
-                                <li>{{ $variable->name }}</li>
-                            @endforeach
-                            </ul></li>
-                            <li>Commands</li>
-                            <li><ul>
-                            @foreach($task->commands as $command)
-                                <li>{{ $command->name }}</li>
-                                <li>{{ $command->command }}</li>
-                                <li><ul>
-                                    @foreach($command->parameters as $parameter)
-                                        <li>{{ $parameter->name }}</li>
-                                        <li>{{ $parameter->value() }}</li>
-                                    @endforeach
-                                </ul></li>
-                            @endforeach
-                            </ul></li>
-                        @endforeach
-                        </ul>
+        <div class="row">
+            <div class="col s12">
+                <ul class="collection with-header blue-grey">
+                    <li class="collection-header">Tasks</li>
+                    @foreach($tasks as $task)
+                        <li class="collection-item">{{ $task->name }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <form class="col s12">
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input placeholder="Placeholder" id="task_name" type="text" class="validate">
+                        <label for="task_name">Description</label>
                     </div>
                 </div>
-            </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <textarea id="task_description" class="materialize-textarea"></textarea>
+                        <label for="task_description">Description</label>
+                    </div>
+                </div>
+                <button class="btn btn-large right waves-effect waves-light" type="submit">Save</button>
+            </form>
         </div>
     </div>
 @endsection
+
+
+<!--
+@foreach($tasks as $task)
+<li>Variables</li>
+                    <li><ul>
+                    @foreach($task->variables as $variable)
+    <li>{{ $variable->name }}</li>
+                    @endforeach
+        </ul></li>
+        <li>Commands</li>
+        <li><ul>
+@foreach($task->commands as $command)
+    <li>{{ $command->name }}</li>
+                        <li>{{ $command->command }}</li>
+                        <li><ul>
+                            @foreach($command->parameters as $parameter)
+        <li>{{ $parameter->name }}</li>
+                                <li>{{ $parameter->value() }}</li>
+                            @endforeach
+            </ul></li>
+@endforeach
+        </ul></li>
+        @endforeach
+        -->
