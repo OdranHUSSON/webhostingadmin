70262d93ed07e75fef2a61f0546768599dc85150
Odran HUSSON <ohusson55@gmail.com>
2019-02-18 11:21:36 +0100

    Config update for travis

--- config/database.php
+++ config/database.php
@@ -61,10 +61,12 @@ return [
             'database'  => env('DB_TEST_DATABASE', 'webhostingadmin'),
             'username'  => env('DB_TEST_USERNAME', 'root'),
             'password'  => env('DB_TEST_PASSWORD', ''),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-            'strict'    => false,
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
         ],
 
         'pgsql' => [
