8f6fa4812963c8f4c2d184767153c95fd443d603
Odran HUSSON <ohusson55@gmail.com>
2019-03-10 17:52:45 +0100

    Add pager to tasks list

--- resources/dashboard/components/Tables/TaskTable.vue
+++ resources/dashboard/components/Tables/TaskTable.vue
@@ -1,12 +1,27 @@
 <template>
-    <div>
-        <md-table v-model="tasks" :table-header-color="tableHeaderColor">
-            <md-table-row slot="md-table-row" slot-scope="{ item }">
-                <md-table-cell md-label="Name">{{ item.name }}</md-table-cell>
-                <md-table-cell md-label="Description">{{ item.description }}</md-table-cell>
-                <md-table-cell md-label="Updated_at">{{ item.updated_at }}</md-table-cell>
-            </md-table-row>
-        </md-table>
+    <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
+        <md-card>
+            <md-card-header data-background-color="red">
+                <h4 class="title">Tasks</h4>
+                <p class="category">Tasks List</p>
+            </md-card-header>
+            <md-card-content>
+                <div>
+                    <md-table v-model="tasks" :table-header-color="tableHeaderColor">
+                        <md-table-row slot="md-table-row" slot-scope="{ item }">
+                            <md-table-cell md-label="Name">{{ item.name }}</md-table-cell>
+                            <md-table-cell md-label="Description">{{ item.description }}</md-table-cell>
+                            <md-table-cell md-label="Updated_at">{{ item.updated_at }}</md-table-cell>
+                        </md-table-row>
+                    </md-table>
+                </div>
+            </md-card-content>
+        </md-card>
+        <div class="pager">
+            <a href="#" v-for="n in pager" v-on:click="updateData(n)">
+                {{ n }}
+            </a>
+        </div>
     </div>
 </template>
 
@@ -28,21 +43,25 @@
                         description: "",
                         updated_at: ""
                     }
-                ]
+                ],
+                pager: 1
             };
         },
-        mounted () {
-            var self = this;
-            axios
-                .get('/api/tasks')
-                .then(function(response) {
-                    response = response.data;
-                    console.log(response);
-                    self.tasks = response.data;
-
-
-                })
-                .catch(error => console.log(error))
+        mounted() {
+            this.updateData(1);
+        },
+        methods : {
+            updateData(page) {
+                var self = this;
+                axios.get('/api/tasks?page='+page)
+                    .then(function (response) {
+                        response = response.data;
+                        console.log(response);
+                        self.tasks = response.data;
+                        self.pager = response.last_page;
+                    })
+                    .catch(error => console.log(error))
+            }
         }
     };
 </script>
--- resources/dashboard/pages/Tasks.vue
+++ resources/dashboard/pages/Tasks.vue
@@ -1,20 +1,5 @@
 <template>
-    <div class="content">
-        <div class="md-layout">
-            <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
-                <md-card>
-                    <md-card-header data-background-color="green">
-                        <h4 class="title">Tasks</h4>
-                        <p class="category">Tasks List</p>
-                    </md-card-header>
-                    <md-card-content>
-                        <task-table table-header-color="green"></task-table>
-                    </md-card-content>
-                </md-card>
-            </div>
-
-        </div>
-    </div>
+    <task-table table-header-color="red"></task-table>
 </template>
 
 <script>
